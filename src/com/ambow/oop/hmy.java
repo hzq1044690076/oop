5925ab5a8dca8963b64b4da9deff5a527bbfa9d6
hmy666 <726415801@qq.com>
2019-10-14 16:50:16 +0800

    新增添了几行

--- src/com/ambow/oop/hmy.java
+++ src/com/ambow/oop/hmy.java
@@ -2,6 +2,8 @@ package com.ambow.oop;
 
 public class hmy {
     public static void main(String[] args) {
-
+        System.out.println("新添加了一行");
+        System.out.println("怎么回退到上一个版本");
+        System.out.println("回退之后 再回退回来，可以吗？");
     }
 }
