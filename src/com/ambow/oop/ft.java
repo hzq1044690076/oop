d2b7c84ab6e7ca1279e6b96d930e0cb0bd73e556
saberfengtao <56355033+saberfengtao@users.noreply.github.com>
2019-10-14 16:45:14 +0800

    Update ft.java

--- src/com/ambow/oop/ft.java
+++ src/com/ambow/oop/ft.java
@@ -4,5 +4,6 @@ public class ft {
     public static void main(String[] args) {
         System.out.println("冯涛");
         System.out.println("新添加一行");
+        System.out.println("在服务器中修改的东西");
     }
 }
